2fec46213d100129fc2c0b8d8ce4cc8aabad8c2f
kamaruddin215280042 <46165329+kamaruddin215280042@users.noreply.github.com>
2018-12-30 23:32:01 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<table>
+
+<td width="298" height="300" align="center">
+<p><strong>Nama : KAMARUDIN</strong></p>
+<p><strong>Nim : 21528042</strong></p>
+<p><strong>Alamat : PINRANG</strong></p>
+<p><strong>Fax : kamaruddinkocak@gmail.com</strong></p>
+<p><strong>Phone : 082188393081</strong></p></td>
+</table>
